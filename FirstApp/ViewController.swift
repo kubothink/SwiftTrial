53b7feb1c9da2480e39f482ed674121c0ca87680
Yutaka Kubota <jaz@kubothink.com>
2015-04-24 02:43:42 +0900

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 久保田　裕 on 2015/04/24.
+//  Copyright (c) 2015年 yui-works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
